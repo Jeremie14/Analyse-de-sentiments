dd1d541529ab9cc41349364a9d56be36e4c53bd3
Jeremie <jerpelletier13@icloud.com>
2026-03-02 22:05:08 -0500

    Ajout du filtrage des mots

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import string
+
 texte = open('lecture.txt', encoding='utf-8').read()
-print(texte)
+petite_lettre = texte.lower()
+texte_nettoye = petite_lettre.translate(str.maketrans('', '', string.punctuation))
+mots_tokenises = texte_nettoye.split()
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+mots_finaux = []
+for mot in mots_tokenises:
+    if mot not in stop_words:
+        mots_finaux.append(mot)
+
+print(mots_finaux)
